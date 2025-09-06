e1c2d10e9a74c40ce5a8103f02213bac552d5086
timothy adigun <adigun.timothy@gmail.com>
2025-09-06 05:00:08 +0100

    make a seq of string upper

---
+++ junkz/Upper.scala
@@ -0,0 +1,8 @@
+
+class Upper1:
+  def convert(str: Seq[String]): Seq[String] =
+    str.map((s: String) => s.toUpperCase())
+
+@main def m() =
+    val upper = new Upper1()
+    println(upper.convert(List("Hello, World")))
