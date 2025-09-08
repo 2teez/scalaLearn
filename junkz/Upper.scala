4151ebc76f0aef039daf55f3c20ef91b47d88815
timothy adigun <adigun.timothy@gmail.com>
2025-09-08 07:50:21 +0100

    practice scala

--- junkz/Upper.scala
+++ junkz/Upper.scala
@@ -1,7 +1,7 @@
 
 class Upper1:
   def convert(str: Seq[String]): Seq[String] =
-    str.map((s: String) => s.toUpperCase())
+    str.map((s: String) => s.toUpperCase)
 
 @main def m() =
     val upper = new Upper1()
