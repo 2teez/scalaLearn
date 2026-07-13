134e1ec7c0fab11628ed23bf46cec0a8ca11292d
timothy adigun <adigun.timothy@gmail.com>
2026-07-13 23:58:34 +0100

    using unary operator in scala

--- programming-in-scala/ch01/Strutils.scala
+++ programming-in-scala/ch01/Strutils.scala
@@ -1,14 +1,18 @@
 package com.progscala3.strutils
-import com.progscala3.strutils.String._
+import com.progscala3.strutils.StringExtentions._
 
 @main def main(args: String*): Unit =
   val str = "scalame"
-  println(str.toTitleCase)
+  println(s"$str in Title case: ${!str}")
+  val str2 = str.toTitleCase
+  println(s"$str2 in Title case: ${!str2}")
+
 
 
 //class Strutils(private val value: String)
 //object Strutils:
   //def apply(value: String): Strutils = new Strutils(value)
-object String:
+object StringExtentions:
   extension (str: String)
     def toTitleCase: String = str.substring(0, 1).toUpperCase + str.substring(1).toLowerCase
+    def unary_! : Boolean = str.toTitleCase == str
