43d4a3dfdb681efe405bcf2084b23bc225363745
timothy adigun <adigun.timothy@gmail.com>
2026-07-13 21:10:05 +0100

    using class object in scala

--- programming-in-scala/ch01/Checksum.scala
+++ programming-in-scala/ch01/Checksum.scala
@@ -1,5 +1,7 @@
 package com.progscala3.checksum
 
+import scala.collection.mutable
+
 @main def main(args: String*): Unit =
   val acc = ChecksumAccumulator()
   val csa = new ChecksumAccumulator
@@ -7,7 +9,25 @@ package com.progscala3.checksum
   csa.add(1)
   println(csa.checksum())
 
+  println(ChecksumAccumulator.calculate("Every value is an object to be considered seriously."))
+
 class ChecksumAccumulator:
   private var sum        = 0
   def add(b: Byte): Unit = sum += b
   def checksum(): Int    = ~(sum & 0xff) + 1
+
+object ChecksumAccumulator:
+  private val cache = mutable.Map.empty[String, Int]
+
+  def calculate(s: String): Int =
+    if cache.contains(s) then
+      cache(s)
+    else
+      val acc = new ChecksumAccumulator
+      for c <- s do
+        acc.add((c >> 8).toByte)
+        acc.add(c.toByte)
+
+      val cs = acc.checksum()
+      cache(s) = cs
+      cs
