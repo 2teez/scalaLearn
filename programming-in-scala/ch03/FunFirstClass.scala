1b0aa0994fef5264a5d98d5efa620fac49ef3203
timothy adigun <adigun.timothy@gmail.com>
2026-07-14 15:14:22 +0100

    using annotation in scala with tailrec

--- programming-in-scala/ch03/FunFirstClass.scala
+++ programming-in-scala/ch03/FunFirstClass.scala
@@ -1,6 +1,8 @@
 package com.progscala3.funfirstclass
 
 import Summation._
+import Recursion._
+import scala.annotation.tailrec
 
 @main def main(args: String*): Unit =
   // using a first-class function
@@ -10,10 +12,29 @@ import Summation._
   display(() => println(result), "Addition: ")
   // using result as a partial function
   // apply the partial function to a value
-  val partial = summation(2, _: Int)
+  val partial = summation(2, _: Int) /// = summation(value, _) // without indicating, type
   display(() => println(partial(8)), "Partial Function: ") //> Partial Function: 10
+  //
+  // using recursion to calculate the Fibonacci sequence
+  display(() => println(factorial(5)), "Factorial: ") //> Factorial: 120
+  val one120 = factorial(_)
+  display(() => println(factorialWithTailRecursion(one120(5))),
+    "Factorial with Tail Recursion: ") //> Factorial with Tail Recursion: 120
 
 object Summation:
   def display(f: () => Unit, label: String): Unit =
     print(label)
     f()
+
+object Recursion:
+  def factorial(n: BigInt): BigInt =
+    if n <= 1 then n
+    else n * factorial(n - 1)
+
+  def factorialWithTailRecursion(n: BigInt): BigInt =
+    @tailrec
+    def factWithTailRecursion(n: BigInt, acc: BigInt): BigInt =
+      if n <= 1 then acc
+      else factWithTailRecursion(n - 1, acc * n)
+
+    factWithTailRecursion(n, 1)
