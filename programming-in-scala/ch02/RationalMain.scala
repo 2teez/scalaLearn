4eb28f7d79b5ee5aa0e911c05324b7b62661ed2b
timothy adigun <adigun.timothy@gmail.com>
2026-07-14 03:14:50 +0100

    using the rational class in main file

--- programming-in-scala/ch02/RationalMain.scala
+++ programming-in-scala/ch02/RationalMain.scala
@@ -3,5 +3,10 @@ package com.progscala3.rationalmain
 import com.progscala3.rational.Rational
 
 @main def main(args: String*): Unit =
-  val rn = Rational(66, 42)
-  println(rn)
+  val x = Rational(1, 2)
+  val y = Rational(2, 3)
+  val z = x + y
+  println(z)
+  //
+  println(x + x * y)
+  println(2 * y + x)
