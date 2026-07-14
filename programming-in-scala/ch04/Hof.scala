b88dc2c59ddcc440a4f9bb95232ecc6bdd7265ca
timothy adigun <adigun.timothy@gmail.com>
2026-07-14 17:58:10 +0100

    using seq and other hof in scala

---
+++ programming-in-scala/ch04/Hof.scala
@@ -0,0 +1,15 @@
+package com.progscala3.hof
+
+import java.io.File
+import FileMatcher._
+
+@main def main(args: String*): Unit =
+  println(filesEnding(".scala").mkString(", "))
+
+object FileMatcher:
+  private def filesGetter: Array[File] = new File(".").listFiles()
+
+  def filesEnding(query: String): Seq[File] =
+    // filesGetter.filter(_.getName.endsWith(query))
+    for file <- filesGetter if file.getName.endsWith(query)
+    yield file
