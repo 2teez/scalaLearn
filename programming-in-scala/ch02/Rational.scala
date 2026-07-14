ff001b86b57d55a2da5b890bcc2dafc2b1c7e248
timothy adigun <adigun.timothy@gmail.com>
2026-07-14 03:13:35 +0100

    using extension in scala file

--- programming-in-scala/ch02/Rational.scala
+++ programming-in-scala/ch02/Rational.scala
@@ -4,7 +4,33 @@ class Rational(val n: Int, val d: Int):
   require(d != 0)
   private val g = gcd(n.abs, d.abs)
   def this(n: Int) = this(n, 1)
+
+  def +(that: Rational): Rational =
+    Rational(this.n * that.d + that.n * this.d, this.d * that.d)
+
+  def +(that: Int): Rational =
+    Rational(this.n + that * this.d, this.d)
+
+  def -(that: Rational): Rational =
+    Rational(this.n * that.d - that.n * this.d, this.d * that.d)
+
+  def -(that: Int): Rational =
+    Rational(this.n - that * this.d, this.d)
+
+  def *(that: Rational): Rational =
+    Rational(this.n * that.n, this.d * that.d)
+
+  def *(that: Int): Rational =
+    Rational(this.n * that, this.d)
+
+  def /(that: Rational): Rational =
+    Rational(this.n * that.d, this.d * that.n)
+
+  def /(that: Int): Rational =
+    Rational(this.n, this.d * that)
+
   override def toString: String = s"Rational(${n / g}/${d / g})"
+
   private def gcd(a: Int, b: Int): Int =
     if b == 0 then a else gcd(b, a % b)
 
@@ -12,3 +38,16 @@ object Rational:
   def apply(n: Int, d: Int): Rational =
     new Rational(n, d)
   def apply(n: Int): Rational         = new Rational(n)
+
+  extension (r: Int)
+    def +(that: Rational): Rational =
+      Rational(r) + that
+
+    def -(that: Rational): Rational =
+      Rational(r) - that
+
+    def *(that: Rational): Rational =
+      Rational(r) * that
+
+    def /(that: Rational): Rational =
+      Rational(r) / that
