dcad679dd874b2b5636720fc9a2420a87d763024
timothy adigun <adigun.timothy@gmail.com>
2026-07-18 01:31:21 +0100

    using traits to mix with classes

--- programming-in-scala/ch06/PracticeTraits.scala
+++ programming-in-scala/ch06/PracticeTraits.scala
@@ -4,13 +4,15 @@ trait Philosophical:
   def philosophize: Unit =
     println("I consume memory, therefore I am!")
 
-trait HasLegs
+trait HasLegs extends Animal
+
 class Animal
-class Frog extends Animal, Philosophical, HasLegs:
+
+class Frog extends Animal, Philosophical, HasLegs
   override def toString: String = "green!"
 
 @main def main(args: String*): Unit =
-  val frog = Frog()
+  val frog                = new Frog()
   frog.philosophize
   val phil: Philosophical = frog
   phil.philosophize
