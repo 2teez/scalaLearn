88c534e3df6cb0c2514406a101a9c1c29a506ee7
timothy adigun <adigun.timothy@gmail.com>
2026-07-18 14:19:07 +0100

    list in scala

--- programming-in-scala/ch08/PracticeCollection.scala
+++ programming-in-scala/ch08/PracticeCollection.scala
@@ -2,4 +2,6 @@ package com.progscala3.practicecollection
 
 @main def main(args: String*): Unit =
   val emptyList: List[Nothing] = List.empty
+  println(emptyList)
+  val fruits = List("apple", "grapes", "orange")
   println(fruits)
