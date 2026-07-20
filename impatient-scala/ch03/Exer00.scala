f7f7b33c49af241454477a8fc85e4df5f1a03cce
timothy adigun <adigun.timothy@gmail.com>
2026-07-20 15:50:38 +0100

    practice some other functions in scala

--- impatient-scala/ch03/Exer00.scala
+++ impatient-scala/ch03/Exer00.scala
@@ -1,4 +1,23 @@
 package com.progscala3.exer00
 
+import java.util.Random
+import Functions.*
+
+package Functions:
+  def minmax(values: Array[Int]): Option[(Int, Int)] =
+    if values.isEmpty then None
+    else Some(values.min, values.max)
+
+  def generateRandomArray(len: Int = 6, size: Int = 10): Array[Int] =
+      val rand = Random()
+      (for _ <- 0 to len yield rand.nextInt(size)).toArray
+
 @main def main(args: String*): Unit =
-  println("Start, Here!")
+  val array = generateRandomArray(len = 0)
+  array.foreach(print)
+  println()
+  println(
+    minmax(Array[Int]()) match
+      case Some(min, max) => s"$min, $max"
+      case _ => "None"
+  )
