6059ec1be73368bc94eab8af48b96d20beecc1c8
timothy adigun <adigun.timothy@gmail.com>
2026-07-21 11:36:15 +0100

    using object , enum and traits in scala

--- impatient-scala/ch04/ObjPractice.scala
+++ impatient-scala/ch04/ObjPractice.scala
@@ -1,4 +1,55 @@
 package com.progscala3.objpractice
 
+import conversions.Conversions.*
+import MeasuringTypes.*
+
+trait Display:
+  def display(msg: String = ""): Unit = println(s"$msg $this")
+  def describe(msg: String = ""): String = s"$msg $this"
+
+enum MeasuringTypes(value: Double, name: String) extends Display:
+
+  case Inches(
+    value: Double,
+    name: String = "inches"
+  ) extends MeasuringTypes(value, name)
+
+  case Centimeters(
+    value: Double,
+    name: String = "centimeters"
+  ) extends MeasuringTypes(value, name)
+
+  case Gallons(
+    value: Double,
+    name: String = "gallons"
+  ) extends MeasuringTypes(value, name)
+
+  case Liters(
+    value: Double,
+    name: String = "liters"
+  ) extends MeasuringTypes(value, name)
+
+  case Miles(
+    value: Double,
+    name: String = "ml") extends MeasuringTypes(value, name)
+
+  case Kilometers(
+    value: Double,
+    name: String = "km") extends MeasuringTypes(value, name)
+
+package conversions:
+  object Conversions:
+
+    def inchesToCentimeters(value: Inches): Centimeters =
+      Centimeters(value.value * 2.54)
+
+    def gallonsToLiters(value: Gallons): Liters =
+      Liters(value.value * 0.22)
+
+    def milesToKilometers(value: Miles): Kilometers =
+      Kilometers(value.value * 1.60934)
+
 @main def main(args: String*): Unit =
-  println("Start, Here!")
+   inchesToCentimeters(Inches(23)).display("23 inches is ")
+   println(gallonsToLiters(Gallons(20.53)).describe("20.53 gallons is equals to"))
+   milesToKilometers(Miles(98)).display("98 miles =")
