99f8216017cbac2f2250e12b7a972967c3750e8e
timothy adigun <adigun.timothy@gmail.com>
2026-07-21 23:47:59 +0100

    practice file reading in scala

--- impatient-scala/ch05/FilePractice.scala
+++ impatient-scala/ch05/FilePractice.scala
@@ -1,12 +1,18 @@
 package com.progscala3.filepractice
 
-import scala.io.Source
+import scala.io.{Source, BufferedSource}
 import com.progscala3.util.Util.*
 import java.io.FileNotFoundException
 
 @main def main(args: String*): Unit =
-   val file = userInput("Enter filename: ").trim()
-   try
-     var fileSource = Source.fromFile(file)
-     for line <- fileSource.getLines() do println(line)
-   catch ex => println(ex)
+  val file = userInput("Enter filename: ").trim()
+  try
+    val fileSource = Source.fromFile(file)
+    try
+      val lines = fileSource.getLines()
+      for line <- lines do println(line)
+    finally
+      fileSource.close()
+  catch
+    case ex: FileNotFoundException => println(ex.getMessage)
+    case ex: Exception             => println(ex.getMessage)
