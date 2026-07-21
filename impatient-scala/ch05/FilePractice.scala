9b9cf8fb9d6acbeadaf9a64e5cd1feceeb7ef652
timothy adigun <adigun.timothy@gmail.com>
2026-07-21 23:11:47 +0100

    file reader in scala

--- impatient-scala/ch05/FilePractice.scala
+++ impatient-scala/ch05/FilePractice.scala
@@ -1,4 +1,12 @@
 package com.progscala3.filepractice
 
+import scala.io.Source
+import com.progscala3.util.Util.*
+import java.io.FileNotFoundException
+
 @main def main(args: String*): Unit =
-  println("Start, Here!")
+   val file = userInput("Enter filename: ").trim()
+   try
+     var fileSource = Source.fromFile(file)
+     for line <- fileSource.getLines() do println(line)
+   catch ex => println(ex)
