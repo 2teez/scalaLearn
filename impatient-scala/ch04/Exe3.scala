d5eb276a9f9e128fca21b7432faa0fec6071d0a8
timothy adigun <adigun.timothy@gmail.com>
2026-07-21 11:57:21 +0100

    inheritance of java.awt.Point

---
+++ impatient-scala/ch04/Exe3.scala
@@ -0,0 +1,8 @@
+package com.progscala3.exe3
+
+// check javadoc for the java.awt.Point
+object Origin extends java.awt.Point
+
+@main def main(args: String*): Unit =
+  val origin = Origin
+  println(origin)
